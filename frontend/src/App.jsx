02ece89fb81094458dd2eff12218cd6c74dc8a32
mdinthisar <inthisarmd@gmail.com>
2025-02-09 22:03:47 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,25 +4,25 @@ import { ToastContainer } from "react-toastify";
 
 import { isLoggedInContext } from "./contexts/isLoggedInContext";
 
-import Home from "./Components/Home";
-import Signup from "./Components/Signup";
-import Login from "./Components/Login";
-import Navbar from "./Components/Navbar";
-import Logout from "./Components/Logout";
-import ForgotPassword from "./Components/ForgotPassword";
-import ResetPassword from "./Components/ResetPassword";
-import CreateProduct from "./Components/CreateProduct";
+import Home from "./Components/Home.jsx";
+import Signup from "./Components/SignUp.jsx";
+import Login from "./Components/Login.jsx.jsx";
+import Navbar from "./Components/Navbar.jsx";
+import Logout from "./Components/Logout.jsx";
+import ForgotPassword from "./Components/ForgotPassword.jsx";
+import ResetPassword from "./Components/ResetPassword.jsx";
+import CreateProduct from "./Components/CreateProduct.jsx";
 // import AuthRoute from "./Components/authRoute";
-import ProtectedRoute from "./Components/ProtectedRoute";
-import Adminproducts from "./Components/AdminProducts";
-import EditProduct from "./Components/EditProduct";
-import Profile from "./Components/Profile";
-import EditProfile from "./Components/EditProfile";
-import ProductDetails from "./Components/ProductDetails";
-import Cart from "./Components/Cart";
-import UserProtectedRoute from "./Components/UserProtectedRoute";
-import TrackOrders from "./Components/TrackOrders";
-import AdminOrders from "./Components/AdminOrders";
+import ProtectedRoute from "./Components/ProtectedRoute.jsx";
+import Adminproducts from "./Components/AdminProducts.jsx";
+import EditProduct from "./Components/EditProduct.jsx";
+import Profile from "./Components/Profile.jsx";
+import EditProfile from "./Components/EditProfile.jsx";
+import ProductDetails from "./Components/ProductDetails.jsx";
+import Cart from "./Components/Cart.jsx";
+import UserProtectedRoute from "./Components/UserProtectedRoute.jsx";
+import TrackOrders from "./Components/TrackOrders.jsx";
+import AdminOrders from "./Components/AdminOrders.jsx";
 
 const App = () => {
   const [isLoggedIn, setisLoggedIn] = useState(false);
