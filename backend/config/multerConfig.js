ade4d4ed6e24ce42c1caa71bce4e031b9f19e6a0
mdinthisar <inthisarmd@gmail.com>
2025-02-12 12:05:01 +0530

    done

--- backend/config/multerConfig.js
+++ backend/config/multerConfig.js
@@ -4,15 +4,17 @@ import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb)=>{
-    cb(null, path.resolve(__dirname, '../admin-products-img'));
-  },
-  filename:(req, file, cb)=>{
-    if(!file.mimetype.startsWith('image')) return cb(new Error('only Image file accepted'), false)
-    cb(null, Date.now()+'.'+file.originalname.split('.').pop())
-  }
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb)=>{
+//     cb(null, path.resolve(__dirname, '../admin-products-img'));
+//   },
+//   filename:(req, file, cb)=>{
+//     if(!file.mimetype.startsWith('image')) return cb(new Error('only Image file accepted'), false)
+//     cb(null, Date.now()+'.'+file.originalname.split('.').pop())
+//   }
+// });
+
+const storage = multer.memoryStorage();
 
 const ownerProductImages = multer({storage});
 
